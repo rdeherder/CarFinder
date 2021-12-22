43e06163deab5b4269779cb8fadc2777244f6bd4
Roland de Herder <macron2000@gmail.com>
2021-12-22 17:03:36 +0100

    feat: Capitalize the first character of the car's make and model

---
+++ CarFinderApi/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace CarFinderApi.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Capitalize(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return value;
+            }
+
+            string result = value[0].ToString().ToUpper();
+            if (value.Length > 1)
+            {
+                result += value[1..];
+            }
+
+            return result;
+        }
+    }
+}
